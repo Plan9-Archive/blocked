c509526c56e7260093cd0d4b2c1f32cf826d8a13
iru <iru@localhost>
2011-08-27 13:34:09 -0300

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,7 @@
+include $(GOROOT)/src/Make.inc
+
+TARG=blocked
+GOFILES=\
+	blocked.go\
+
+include $(GOROOT)/src/Make.cmd
---
+++ blocked.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"goplan9.googlecode.com/hg/plan9/acme"
+)
+
+// Assumes a 64 byte line
+func block2body(path string, w *acme.Win) {
+	f, err := os.Open(path)
+	defer f.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not open file %s: %s\n", flag.Arg(0), err)
+		os.Exit(1)
+	}
+
+	var line [64]byte
+	for {
+		n, err := f.Read(line[:])
+		if err == os.EOF {
+			break
+		}
+		b := line[:n]
+		w.Printf("body", "%s\n", b)
+	}
+}
+
+
+func body2block(w *acme.Win, path string) {
+	f, err := os.OpenFile(path, os.O_WRONLY, 0)
+	defer f.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not open file %s: %s\n", flag.Arg(0), err)
+		os.Exit(1)
+	}
+
+	b, err := w.ReadAll("body")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not read acme data file: %s\n", err)
+		os.Exit(1)
+	}
+
+	lines := strings.FieldsFunc(string(b), func (c int) bool { return c == '\n' })
+	for _, line := range lines {
+		f.WriteString(line + strings.Repeat(" ", 64-len(line)))
+	}
+}
+	
+func main() {
+	flag.Parse()
+
+	w, err := acme.New()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error creating window: %s\n", err)
+		os.Exit(1)
+	}
+
+	cwd, _ := os.Getwd()
+	w.Name(cwd + "/" + flag.Arg(0))
+	block2body(flag.Arg(0), w)
+
+	c := w.EventChan()
+	for {
+		ev, ok := <-c
+		if !ok {
+			break
+		}
+
+		if ev.C1 == 'M' && ev.C2 == 'x' && string(ev.Text[:3]) == "Put" {
+			body2block(w, flag.Arg(0))
+		} else {
+			w.WriteEvent(ev)
+		}
+	}
+	fmt.Printf("closed!\n")
+}
